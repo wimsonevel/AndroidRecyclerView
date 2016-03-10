3782b0b06b34b65a6d4fd567f58c91db86d14581
Wim Sonevel <wim.sonevel@yahoo.co.id>
2016-03-10 09:13:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wim/androidrecyclerview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.wim.androidrecyclerview;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wim.androidrecyclerview.fragments.MemberFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(android.R.id.content);
+        if(fragment == null){
+            fragment = MemberFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, fragment, "")
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .attach(fragment)
+                    .commit();
+        }
+    }
+}
